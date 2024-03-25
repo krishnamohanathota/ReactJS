e3009666e2275aa9372137e713c8b89b031ab0b3
Krishna Mohan <krishnamohan.athota@gmail.com>
2024-03-24 22:17:27 -0500

    React JS - React Router

---
+++ reactjs/example14-reactrouter/worldwise/src/components/Map.jsx
@@ -0,0 +1,19 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+import styles from "./Map.module.css";
+
+export default function Map() {
+  const navigate = useNavigate();
+  const [searchParam, setSearchparam] = useSearchParams();
+
+  const lat = searchParam.get("lat");
+  const lng = searchParam.get("lng");
+
+  return (
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+      <h1>Map</h1>
+      <h2>
+        Position : {lat},{lng}
+      </h2>
+    </div>
+  );
+}
