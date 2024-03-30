80824eba50c5e27b7af510a4f528a3722a8c7901
Krishna Mohan <krishnamohan.athota@gmail.com>
2024-03-30 13:24:11 -0500

    Old directories are removed

--- reactjs/example16-contextapi/worldwise/src/hooks/useUrlPosition.jsx
+++
@@ -1,9 +0,0 @@
-import { useSearchParams } from "react-router-dom";
-
-export function useUrlPosition() {
-  const [searchParam] = useSearchParams();
-  const lat = searchParam.get("lat");
-  const lng = searchParam.get("lng");
-
-  return { lat, lng };
-}
