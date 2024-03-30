ab6b54f85c88df315a9dd3d4cde039c1758bd1f9
Krishna Mohan <krishnamohan.athota@gmail.com>
2024-03-29 23:32:11 -0500

    Context API and useReducer

---
+++ reactjs/example16-contextapi/worldwise/src/components/CityItem.jsx
@@ -0,0 +1,40 @@
+import { useCities } from "../contexts/CityContext";
+import styles from "./CityItem.module.css";
+import { Link } from "react-router-dom";
+
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(new Date(date));
+
+export default function CityItem({ city }) {
+  const { currentCity, deleteCity } = useCities();
+
+  const { cityName, country, emoji, date, id, position } = city;
+
+  async function handleDeleteCity(e) {
+    e.preventDefault();
+    console.log("TEST");
+    await deleteCity(id);
+  }
+
+  return (
+    <li>
+      <Link
+        className={`${styles.cityItem} ${
+          currentCity.id === id ? styles["cityItem--active"] : ""
+        }`}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
+        <span className={styles.emoji}>{emoji}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>({formatDate(date)})</time>
+        <button className={styles.deleteBtn} onClick={handleDeleteCity}>
+          &times;
+        </button>
+      </Link>
+    </li>
+  );
+}
